80e32edb033258351d16a1e0d9e398732f571b51
marcodes <marcodesanctis2@gmail.com>
2019-11-20 11:29:29 +0000

    Unit tests

--- src/Des.Blazor.Authorization.Msal/Extensions/MsalConfigurationExtensions.cs
+++ src/Des.Blazor.Authorization.Msal/Extensions/MsalConfigurationExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Des.Blazor.Authorization.Msal;
+using Des.Blazor.Authorization.Msal.Utils;
 using Microsoft.AspNetCore.Components.Authorization;
 using System;
 using System.Threading.Tasks;
@@ -20,6 +21,8 @@ namespace Microsoft.Extensions.DependencyInjection
 
         public static IServiceCollection AddAzureActiveDirectory(this IServiceCollection services, IMsalConfig config)
         {
+            Guard.ThrowIfNull(config, nameof(config));
+
             services.AddAzureActiveDirectory(sp => Task.FromResult(config));
 
             return services;
@@ -27,6 +30,8 @@ namespace Microsoft.Extensions.DependencyInjection
 
         public static IServiceCollection AddAzureActiveDirectory(this IServiceCollection services, Func<IServiceProvider, Task<IMsalConfig>> configurator)
         {
+            Guard.ThrowIfNull(configurator, nameof(configurator));
+
             services
                 .AddBaseServices()
                 .AddScoped<IConfigProvider<IMsalConfig>>(sp => 
---
+++ src/Tests/Des.Blazor.Authorization.Msal.Tests/MsalConfigurationExtensions_Tests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Des.Blazor.Authorization.Msal.Tests
+{
+    public class MsalConfigurationExtensions_Tests
+    {
+        [Fact]
+        public void AddAzureActiveDirectory_WithNullServicesAndConfig_ThrowsArgumentNullException()
+        {
+            IServiceCollection services = null;
+            IMsalConfig config = null;
+
+            Assert.Throws<ArgumentNullException>(() => services.AddAzureActiveDirectory(config));
+        }
+
+        [Fact]
+        public void AddAzureActiveDirectory_WithNullServicesAndConfigurator_ThrowsArgumentNullException()
+        {
+            IServiceCollection services = null;
+            Func<IServiceProvider, Task<IMsalConfig>> configurator = null;
+
+            Assert.Throws<ArgumentNullException>(() => services.AddAzureActiveDirectory(configurator));
+        }
+
+        [Fact]
+        public void AddAzureActiveDirectory_WithNullConfig_ThrowsArgumentNullException()
+        {
+            var servicesMock = new Mock<IServiceCollection>();
+            IServiceCollection services = servicesMock.Object;
+            IMsalConfig config = null;
+
+            Assert.Throws<ArgumentNullException>(() => services.AddAzureActiveDirectory(config));
+        }
+
+        [Fact]
+        public void AddAzureActiveDirectory_WithNullFunction_ThrowsArgumentNullException()
+        {
+            var servicesMock = new Mock<IServiceCollection>();
+            IServiceCollection services = servicesMock.Object;
+            Func<IServiceProvider, Task<IMsalConfig>> configurator = null;
+
+            Assert.Throws<ArgumentNullException>(() => services.AddAzureActiveDirectory(configurator));
+        }
+    }
+}
