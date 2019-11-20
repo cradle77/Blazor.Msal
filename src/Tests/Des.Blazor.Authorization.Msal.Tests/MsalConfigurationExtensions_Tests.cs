eacbb3ad23cdfae69f095cb94501bef050e7e851
marcodes <marcodesanctis2@gmail.com>
2019-11-20 12:16:52 +0000

    MsalConfigurationExtensions tests

--- src/Tests/Des.Blazor.Authorization.Msal.Tests/MsalConfigurationExtensions_Tests.cs
+++ src/Tests/Des.Blazor.Authorization.Msal.Tests/MsalConfigurationExtensions_Tests.cs
@@ -1,6 +1,12 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.JSInterop;
 using Moq;
 using System;
+using System.Linq;
+using System.Net.Cache;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -37,13 +43,52 @@ namespace Des.Blazor.Authorization.Msal.Tests
         }
 
         [Fact]
-        public void AddAzureActiveDirectory_WithNullFunction_ThrowsArgumentNullException()
+        public void AddAzureActiveDirectory_WithNullConfigurator_ThrowsArgumentNullException()
         {
             var servicesMock = new Mock<IServiceCollection>();
             IServiceCollection services = servicesMock.Object;
             Func<IServiceProvider, Task<IMsalConfig>> configurator = null;
 
             Assert.Throws<ArgumentNullException>(() => services.AddAzureActiveDirectory(configurator));
+        }
+
+        [Fact]
+        public void AddAzureActiveDirectory_WithConfigurator_RegistersServices()
+        {
+            var services = new ServiceCollection();
+            Func<IServiceProvider, Task<IMsalConfig>> configurator = 
+                (sp) => Task.FromResult((IMsalConfig)null);
+
+            services.AddAzureActiveDirectory(configurator);
+
+            Assert.Contains(services, s => s.ServiceType == typeof(AuthenticationStateProvider));
+            Assert.Contains(services, s => s.ServiceType == typeof(MsalAuthenticationStateProvider));
+            Assert.Contains(services, s => s.ServiceType == typeof(IConfigProvider<IMsalConfig>));
+        }
+
+        [Fact]
+        public void AddAzureActiveDirectory_RegistersSameInstances_ForStateProvider()
+        {
+            var services = new ServiceCollection();
+
+            var jsRuntime = new Mock<IJSRuntime>();
+            services.AddTransient(sp => jsRuntime.Object);
+            var httpClient = new Mock<HttpClient>();
+            services.AddTransient(sp => httpClient.Object);
+            var navigationManager = new Mock<NavigationManager>();
+            services.AddTransient(sp => navigationManager.Object);
+
+            Func<IServiceProvider, Task<IMsalConfig>> configurator =
+                (sp) => Task.FromResult((IMsalConfig)null);
+
+            services.AddAzureActiveDirectory(configurator);
+
+            var provider = services.BuildServiceProvider();
+
+            var msalAuthenticationState = provider.GetService<MsalAuthenticationStateProvider>();
+            var authenticationState = provider.GetService<AuthenticationStateProvider>();
+
+            Assert.Same(msalAuthenticationState, authenticationState);            
         }
     }
 }
