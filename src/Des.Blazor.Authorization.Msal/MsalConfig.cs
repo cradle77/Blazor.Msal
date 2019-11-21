0286705664bdb2d5e2959e425a82d9eb74291109
marcodes <marcodesanctis2@gmail.com>
2019-11-21 18:21:35 +0000

    Uri initialization

---
+++ src/Des.Blazor.Authorization.Msal/MsalConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace Des.Blazor.Authorization.Msal
+{
+    internal class MsalConfig : IMsalConfig
+    {
+        public string ClientId { get; set; }
+
+        public string Authority { get; set; }
+
+        public LoginModes LoginMode { get; set; }
+    }
+}
